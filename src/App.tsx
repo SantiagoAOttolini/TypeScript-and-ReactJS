400a0f2af2f1514f075ef1b7047092d450a804e7
santiago <santiago.ottolini97@hotmail.com>
2021-04-29 09:06:52 -0300

    TS-React: Add bootswatch

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,43 @@ import React, { useState } from "react";
 type formElement = React.FormEvent<HTMLFormElement>;
 
 interface ITask {
-  name: string,
-  done: boolean
+  name: string;
+  done: boolean;
 }
 
 //Return an element
 function App(): JSX.Element {
-
   const [newTask, setNewTask] = useState<string>("");
-  const [tasks, setTask] = useState<ITask[]>([])
+  //List of task
+  const [tasks, setTask] = useState<ITask[]>([]);
 
   const handleSubmit = (e: formElement) => {
     e.preventDefault();
-    addTask(newTask)
+    addTask(newTask);
+    //When i send a data, clean the input
+    setNewTask("");
   };
 
   //Recive a name of a new task
-  const addTask = (name:string) =>{
-    //Copy the new content of tasks and add a new one 
-    const newTasks: ITask[] = [...tasks, {name: name, done: false} ]
-    setTask(newTasks)
-  }
+  const addTask = (name: string) => {
+    //Copy the new content of tasks and add a new one
+    const newTasks: ITask[] = [...tasks, { name: name, done: false }];
+    setTask(newTasks);
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
         <button>Save</button>
       </form>
+      {tasks.map((t: ITask, i: number) => {
+        return <h1 key={i}>{t.name}</h1>;
+      })}
     </div>
   );
 }
