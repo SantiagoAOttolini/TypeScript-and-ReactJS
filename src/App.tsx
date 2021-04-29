ee87bb8c7ee8e99a3c0f09c245e8a0200cc29e78
santiago <santiago.ottolini97@hotmail.com>
2021-04-29 09:39:40 -0300

    TS-React: Delete task

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,28 @@ function App(): JSX.Element {
   };
 
   //Recive a name of a new task
-  const addTask = (name: string) => {
+  const addTask = (name: string): void => {
     //Copy the new content of tasks and add a new one
     const newTasks: ITask[] = [...tasks, { name: name, done: false }];
     setTask(newTasks);
   };
+
+  const toggleDoneTask = (i: number): void => {
+    //copy in a new variable the actual value of the task inside the state
+    const newTasks: ITask[] = [...tasks];
+    /*
+    if of all the tasks I have a task with value 
+    true it is changed to false and the list with the object changes*/
+    newTasks[i].done = !newTasks[i].done;
+    setTask(newTasks);
+  };
+
+  const removeTask = (i: number): void =>{
+    const newTasks: ITask[] = [...tasks]
+    /*of all the tasks I delete a task with the index that they pass me by parameter*/
+    newTasks.splice(i, 1)
+    setTask(newTasks)
+  }
 
   return (
     <div className="container p-4">
@@ -40,14 +57,30 @@ function App(): JSX.Element {
                   onChange={(e) => setNewTask(e.target.value)}
                   value={newTask}
                   className="form-control"
+                  autoFocus
                 />
                 <button className="btn btn-success btn-block mt-2">Save</button>
               </form>
             </div>
           </div>
-          {tasks.map((t: ITask, i: number) => {
-            return <h1 key={i}>{t.name}</h1>;
-          })}
+          {tasks.map((t: ITask, i: number) => (
+            <div className="card card-body mt-2" key={i}>
+              <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                {t.name}
+              </h2>
+              <div>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => toggleDoneTask(i)}
+                >
+                  {t.done ? "✓" : "✗"}
+                </button>
+                <button className="btn btn-danger" onClick = {()=> removeTask(i)}>
+                🗑
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
