d61ec68c0521631a6b75f171861d9c0c8e1ab4e7
santiago <santiago.ottolini97@hotmail.com>
2021-04-29 09:14:37 -0300

    TS-React: Style form

--- src/App.tsx
+++ src/App.tsx
@@ -29,18 +29,27 @@ function App(): JSX.Element {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setNewTask(e.target.value)}
-          value={newTask}
-        />
-        <button>Save</button>
-      </form>
-      {tasks.map((t: ITask, i: number) => {
-        return <h1 key={i}>{t.name}</h1>;
-      })}
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  onChange={(e) => setNewTask(e.target.value)}
+                  value={newTask}
+                  className="form-control"
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+            </div>
+          </div>
+          {tasks.map((t: ITask, i: number) => {
+            return <h1 key={i}>{t.name}</h1>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
